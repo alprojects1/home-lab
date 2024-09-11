081819af64dccbb2ef12ee764b8ed7127110f52a
Al <77207882+alprojects1@users.noreply.github.com>
2024-09-11 08:12:05 -0500

    Added a rb file for gitlab
    I prefer this vs hard coding into yaml config

--- gitlab/gitlab.rb
+++ gitlab/gitlab.rb
@@ -1,1 +1,15 @@
+# URL link
+external_url 'https://gitlabs.alprojects.org'
+
+# SSH port
+gitlab_rails['gitlab_shell_ssh_port'] = 3200
+
+
+# Disabling Gitlab Process
+letsencrypt['auto_renew'] = false
+
+
+# Traefik will terminate
+nginx['listen_port'] = 80
+nginx['listen_https'] = false
 
