7fd047aa2331c5c1e4348d23a3e2fc5daec9d46a
Al <77207882+alprojects1@users.noreply.github.com>
2024-09-17 01:54:19 -0500

    added py demo
    created simple .py

---
+++ python/demo.py
@@ -0,0 +1,6 @@
+# ==================================================
+def print_pylife_pycharm_pylife (name):
+    print(f'pylife pycharm pylife {name}')
+
+if __name__ == '__main__':
+    print_pylife_pycharm_pylife('pyheff')
