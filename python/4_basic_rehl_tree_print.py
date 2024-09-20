42a4010068703187350c96f06162cd341d516125
Al <77207882+alprojects1@users.noreply.github.com>
2024-09-20 14:14:16 -0500

    Create 4_basic_rehl_tree_print.py
    Added a pointed chart

---
+++ python/4_basic_rehl_tree_print.py
@@ -0,0 +1,15 @@
+def print_linux_tree():
+    print("              Red Hat Linux")
+    print("                   |")
+    print("                   v")
+    print("        Red Hat Enterprise Linux")
+    print("|-------------|     |     |-------------|")
+    print("|             |     |                   |")
+    print("CentOS (X)    |     |                   |")
+    print("|--(spiritual)|-----|                   |")
+    print("v             v     v                   v")
+    print("CentOS Stream Alma  Rocky          Oracle")
+
+
+if __name__ == "__main__":
+    print_linux_tree()4_basic_rehl_tree_print.py
