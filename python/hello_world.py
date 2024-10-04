7d3101076fdf4396ff5f9971191e21b5d2dc5702
LR03 <alporjects1@proton.me>
2024-10-04 07:21:40 -0500

    moved python into its own repo

--- python/hello_world.py
+++
@@ -1,2 +0,0 @@
-#This directly prints the string "Hello, World!" to the console without using any functions or encapsulation
-print("Hello, World!")
--- python/static_single_function.py
+++
@@ -1,6 +0,0 @@
-# define a function called print_pylife_pycharm_pylife that takes an argument name + block checking
-def print_pylife_pycharm_pylife (name):
-    print(f'pylife pycharm pylife {name}')
-
-if __name__ == '__main__':
-    print_pylife_pycharm_pylife('pyheff')
